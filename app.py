a238b57f4b133e5437ad2a2d7433736e5fd047ad
Trofimov <mikletrof@gmail.com>
2021-12-17 14:49:20 +0300

    added flask, get and post methods

---
+++ app.py
@@ -0,0 +1,25 @@
+import threading
+
+from flask import Flask, jsonify, request
+
+from Water_counter import Water_counter
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/water', methods=['GET'])
+def get_water():
+    return jsonify({'hot': Water_counter.hot,
+                    'cold': Water_counter.cold})
+
+
+@app.route('/api/v1/water', methods=['POST'])
+def set_water():
+    req = request.get_json()
+    Water_counter.hot = req["hot"]
+    Water_counter.cold = req["cold"]
+    return jsonify({'water': True})
+
+
+if __name__ == "__main__":
+    threading.Thread(target=lambda: app.run(debug=True, use_reloader=False)).start()
